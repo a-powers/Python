d93ce335bef08b38d702793188ae14adf9bfc1c4
a-powers <awpowers@gmail.com>
2020-05-28 22:07:29 -0400

    update

--- the-while-loop.py
+++ the-while-loop.py
@@ -20,17 +20,17 @@ while count <= 5:
 
 
 
-# def fizzbuzz(numberz):
-#     numbers = 1
+def fizzbuzz(numberz):
+    numbers = 1
 
-#     while numbers < numberz:
-#         if numbers % 3 == 0 and numbers % 5 == 0:
-#             print("FizzBuzz")
-#         elif numbers % 3 == 0:
-#             print("Fizz")
-#         elif numbers % 5 == 0:
-#             print("Buzz")
-#         else:
-#             print(numbers)
-#         numbers += 1
-# fizzbuzz(300)
+    while numbers < numberz:
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            print("FizzBuzz")
+        elif numbers % 3 == 0:
+            print("Fizz")
+        elif numbers % 5 == 0:
+            print("Buzz")
+        else:
+            print(numbers)
+        numbers += 1
+fizzbuzz(300)
